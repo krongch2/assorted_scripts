35b51d6f218e2fb6a12d7704d470954074824064
Mick Krongchon <krongch2@illinois.edu>
2018-05-01 04:23:40 -0500

    update

--- pokdeng.py
+++ pokdeng.py
@@ -2,8 +2,9 @@ import numpy as np
 import pandas as pd
 import random
 
-suits = ['♠', '♦', '♥', '♣']
+suits = ['♣', '♦', '♥', '♠']
 kinds = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+face_kinds = ['J', 'Q', 'K']
 
 class Card():
 
@@ -18,7 +19,7 @@ class Card():
     def card_value(self):
         if self.kind == 'A':
             value = 1
-        elif self.kind in ['J', 'Q', 'K']:
+        elif self.kind in face_kinds:
             value = 10
         else:
             value = int(self.kind)
@@ -29,16 +30,10 @@ class Card():
 
 class Hand():
 
-    def __init__(self, name_list):
-        cards = []
-        names = []
-        for name in name_list:
-            c = Card(name)
-            cards.append(c)
-            names.append(c.name)
-        self.cards = cards
-        self.n = len(cards)
+    def __init__(self, names):
+        self.cards = [Card(name) for name in names]
         self.names = names
+        self.n = len(names)
         self.value = self.hand_value()
         self.rank = self.hand_rank()
         self.rank_order = self.hand_rank_order()
@@ -48,12 +43,11 @@ class Hand():
         return sum(card.value for card in self.cards) % 10
 
     def hand_rank(self):
-        face_kinds = ['J', 'Q', 'K']
         if self.n == 2 and self.value in [8, 9]:
             return 'pok'
         elif self.n == 3 and (self.cards[0].kind == self.cards[1].kind == self.cards[2].kind):
             return 'tong'
-        elif self.n == 3 and (self.cards[0]['kind'] in face_kinds and self.cards[1]['kind'] in face_kinds and self.cards[2]['kind'] in face_kinds):
+        elif self.n == 3 and (self.cards[0].kind in face_kinds and self.cards[1].kind in face_kinds and self.cards[2].kind in face_kinds):
             return 'face'
         else:
             return 'normal'
@@ -68,85 +62,65 @@ class Hand():
         elif self.rank == 'face':
             return 3
         else:
-            if self.n == 2 and (self.cards[0]['kind'] == self.cards[1]['kind'] or self.cards[0]['suit'] == self.cards[1]['suit']):
+            if self.n == 2 and (self.cards[0].kind == self.cards[1].kind or self.cards[0].suit == self.cards[1].suit):
                 return 2
-            elif self.n == 3 and (self.cards[0]['suit'] == self.cards[1]['suit'] == self.cards[2]['suit']):
+            elif self.n == 3 and (self.cards[0].suit == self.cards[1].suit == self.cards[2].suit):
                 return 3
             else:
                 return 1
 
+    def draw(self, name):
+        self.cards.append(Card(name))
+        self.names.append(name)
+        self.n += 1
+        self.value = self.hand_value()
+        self.rank = self.hand_rank()
+        self.rank_order = self.hand_rank_order()
+        self.deng = self.hand_deng()
+
     def __str__(self):
-        return f'[{" ".join(self.names)}], Value: {self.value}, Rank: {self.rank}, Deng: {self.deng}'
+        return f'[{" ".join(self.names):<11}], Rank: {self.rank:>6}, Value: {self.value}, Deng: {self.deng}'
 
-h = Hand(['K♣', 'K♦', 'K♥'])
-print(h)
-
-# def str2card(s):
-#     kind = s[:-1]
-#     suit = s[-1]
-#     return {
-#         'kind': kind,
-#         'suit': suit,
-#         'value': card_value(kind)
-#     }
-# def c(l):
-#     # convert hand string to hand dict
-#     return [str2card(card) for card in l]
-
-# def card2str(card):
-#     return f'{card["kind"]}{card["suit"]}'
-
-def compare_hands(hand1, hand2):
+def compare(hand1, hand2):
     if hand1.rank_order > hand2.rank_order:
-        return 'Win', hand1.deng
+        return hand1.deng
     elif hand1.rank_order < hand2.rank_order:
-        return 'Lose', hand2.deng
+        return -hand2.deng
     else:
         if hand1.value > hand2.value:
-            return 'Win', hand1.deng
+            return hand1.deng
         elif hand1.value < hand2.value:
-            return 'Lose', hand2.deng
+            return -hand2.deng
         else:
             if hand1.deng > hand2.deng:
-                return 'Win', hand1.deng - hand2.deng
+                return hand1.deng - hand2.deng
             elif hand1.deng < hand2.deng:
-                return 'Lose', hand2.deng - hand1.deng
+                return -(hand2.deng - hand1.deng)
             else:
-                return 'Tie', 0
+                return 0
 
-# deck = [{'kind': kind, 'suit': suit, 'value': card_value(kind)} for kind in kinds for suit in suits]
-test_hands = [
-    ['J♦', '9♥'],
-    ['9♠', '9♦'],
-    ['7♠', '7♥', '7♣'],
-    ['K♣', 'K♦', 'K♥'],
-    ['Q♣', 'J♣', 'J♦'],
-    ['J♠', 'Q♠', 'K♠'],
-    ['A♣', '4♣'],
-    ['5♥', '7♥', '7♠']
-]
+def round(deck, nplayers):
+    cutoff_value = 4
+    chunks = [deck[i:i+2] for i in range(0, len(deck), 2)]
+    hands = [Hand(chunk) for chunk in chunks[:nplayers]]
+    topdeck_i = 2 * nplayers
+    result = []
+    for hand in hands:
+        if hand.value <= cutoff_value:
+            hand.draw(deck[topdeck_i])
+            topdeck_i += 1
+        result.append(compare(hand, hands[0]))
+        # print(hand, compare(hand, hands[0]))
+    result[0] = -sum(result[1:])
+    return result
 
-for test_hand in test_hands:
-    print(Hand(test_hand))
+def sim(nrounds, nplayers):
+    deck = [f'{kind}{suit}' for kind in kinds for suit in suits]
+    results = []
+    for _ in range(nrounds):
+        results.append(round(random.sample(deck, len(deck)), nplayers))
+    net = [sum(i) for i in zip(*results)]
+    print(net)
+    return net
 
-# d = pd.DataFrame({'hand_str': test_hands})
-# d['hand_dict'] = d['hand_str'].apply(c)
-# d['hand_rank'] = d['hand_dict'].apply(hand_rank)
-# d['hand_value'] = d['hand_dict'].apply(hand_value)
-# d['hand_deng'] = d['hand_dict'].apply(hand_deng)
-# print(d)
-
-# print(compare_hands(c(['J♦', '9♥']), c(['9♠', '9♦'])))
-# player1 = []
-# player2 = []
-# shuffled = random.sample(deck, len(deck))
-# player1 = shuffled[0:2]
-# player2 = shuffled[2:4]
-# print(player1)
-# print(player2)
-# print(compare_hands(shuffled[0], shuffled[1]))
-# for card in shuffled:
-#     if len(player1) == 2 or len(player2) == 2:
-#         break
-
-#     print(card2str(card))
+sim(10000, 10)
